9e9e35c97f52046157d24aa8a83b094344b4a1b2
i-ankit-here <closetooankit@gmail.com>
2024-07-08 23:50:21 +0530

    optimaal solution by Dutch National Flag Algorithm

--- ArrayQuestionsMedium/majority_element.java
+++ ArrayQuestionsMedium/majority_element.java
@@ -5,17 +5,40 @@ public class majority_element {
     // Count frequence of each element and return if it is>N/2
 
 
-    
+
     // ************* BETTER SOLUTION USING HASH ARRAY ******************
+    // public static void main(String[] args) {
+    //     int[] arr = {2,2,3,3,4,3,3,3,5,3,6,3,7,3,3};
+    //     int[] hash = new int[100];
+    //     Arrays.fill(hash, 0);
+    //     for(int i=0;i<arr.length;i++){
+    //         hash[arr[i]]+=1;
+    //     }
+    //     for(int i=0;i<hash.length;i++){
+    //         if(hash[i]>(arr.length/2)){System.out.println(i);break;}
+    //     }
+    // }
+
+
+    // ******************** OPTIMAL SOLUTION ***************************
+    // Case when the majority element is always present
+    // when it is not we need to use an extra loop to count the frequeny of elem at last and return it if is (>N/2)
     public static void main(String[] args) {
         int[] arr = {2,2,3,3,4,3,3,3,5,3,6,3,7,3,3};
-        int[] hash = new int[100];
-        Arrays.fill(hash, 0);
-        for(int i=0;i<arr.length;i++){
-            hash[arr[i]]+=1;
+        int count =0;
+        int elem = arr[0];
+        for (int i = 0;i< arr.length; i++){
+            if(elem == arr[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+            if(count<=0){
+                elem=arr[i+1];
+            }
         }
-        for(int i=0;i<hash.length;i++){
-            if(hash[i]>(arr.length/2)){System.out.println(i);break;}
-        }
+        System.out.println(elem);
+        
     }
 }
